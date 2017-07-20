c9e7bc93cea11c351b6491340c0cddeedaa02973
sunchao <htfr@live.cn>
2017-07-20 19:43:26 +0800

    测试mocha

---
+++ Test/mocha/add.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sunchao on 2017/07/19.
+ */
+function add(x, y) {
+    return  x+y;
+}
+module.exports = add;
+
---
+++ Test/mocha/add.test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sunchao on 2017/07/19.
+ */
+var add = require('./add.js');
+var expect = require('chai').expect;
+
+describe('加法函数的测试', function() {
+    it('1 加 1 应该等于 2', function() {
+        expect(add(1, 1)).to.be.equal(2);
+    });
+});
