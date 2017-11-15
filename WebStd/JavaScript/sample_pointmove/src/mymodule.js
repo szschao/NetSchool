ec75ad4da265e5dddd92eb9370383cba7b533801
sunchao <htfr@live.cn>
2017-11-15 14:38:52 +0800

    My Fist Sample

---
+++ WebStd/JavaScript/sample_pointmove/src/mymodule.js
@@ -0,0 +1,32 @@
+var minimatch = require('minimatch');
+
+var helper = require('../lib/helper');
+var echo = require('../lib/echo');
+var optional = require('../lib/optional');
+
+module.exports = {
+
+  add: function(a, b) {
+    return a + b;
+  },
+
+  mult: function(a, b) {
+    return helper.mult(a, b);
+  },
+
+  isJs: function(filename) {
+    return minimatch(filename, '*.js');
+  },
+
+  callCallback: function(cb) {
+    return cb(1302);
+  },
+
+  echo: function(str) {
+    return echo.echo(str);
+  },
+
+  useOptional: function() {
+    return optional.optional();
+  },
+};
